973f5108c8923a5be39732905cfa55776db08bec
junnatun <junsa02@gmail.com>
2023-12-11 21:52:31 +0700

    get user data on profile

--- app/src/main/java/com/imajunna/noonaapp/ui/Profile.kt
+++ app/src/main/java/com/imajunna/noonaapp/ui/Profile.kt
@@ -1,10 +1,18 @@
 package com.imajunna.noonaapp.ui
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.imajunna.noonaapp.R
 import com.imajunna.noonaapp.databinding.ActivityLessonPageBinding
 import com.imajunna.noonaapp.databinding.ActivityLessonsBinding
@@ -12,10 +20,13 @@ import com.imajunna.noonaapp.databinding.ActivityProfileBinding
 
 class Profile : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        db = Firebase.firestore
+
         binding.btnEdit.setOnClickListener {
             startActivity(Intent(this, ProfileEdit::class.java))
             finish()
@@ -23,11 +34,63 @@ class Profile : AppCompatActivity() {
         binding.btnLogout.setOnClickListener {
             signout()
         }
+
+        //GET USER EMAIL FROM AUTH
+        val userEmail = FirebaseAuth.getInstance().currentUser?.email
+
+        val docRef = db.collection("users").document(userEmail!!)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    var data = document.data
+
+                    //convert to string using gson
+                    val gson = Gson()
+                    val jsonData = gson.toJson(data)
+
+                    //save to local
+                    saveUserData(jsonData)
+
+                    var nama = data!!["nama"].toString()
+                    var email = data!!["email"].toString()
+                    var tglLahir = data!!["tgl_lahir"].toString()
+                    var beratBadan = data!!["berat_badan"].toString()
+                    var tinggiBadan = data!!["tinggi_badan"].toString()
+
+
+                    if (nama != "") {
+                        binding.textNama.text = nama
+                    }
+                    if (email != "") {
+                        binding.textEmail.text = email
+                    }
+                    if (tglLahir != "") {
+                        binding.textTglLahir.text = tglLahir
+                    }
+                    if (beratBadan != "") {
+                        binding.textBeratBadan.text = beratBadan
+                    }
+                    if (tinggiBadan != "") {
+                        binding.textTinggiBadan.text = tinggiBadan
+                    }
+                }
+            }
+
+
     }
 
     private fun signout() {
         Firebase.auth.signOut()
         startActivity(Intent(this, LoginPage::class.java))
         finish()
+    }
+
+    private fun saveUserData(data: String) {
+        val sharedPreferences: SharedPreferences =
+            getSharedPreferences("localData", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+        editor.putString("userData", data)
+        editor.apply()
     }
 }
--- app/src/main/java/com/imajunna/noonaapp/ui/ProfileEdit.kt
+++ app/src/main/java/com/imajunna/noonaapp/ui/ProfileEdit.kt
@@ -1,8 +1,12 @@
 package com.imajunna.noonaapp.ui
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.imajunna.noonaapp.R
 import com.imajunna.noonaapp.databinding.ActivityProfileBinding
 import com.imajunna.noonaapp.databinding.ActivityProfileEditBinding
@@ -16,12 +20,40 @@ class ProfileEdit : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSimpan.setOnClickListener {
-            startActivity(Intent(this, Profile::class.java))
-            finish()
+            updateProfil()
         }
         binding.btnBatal.setOnClickListener {
             startActivity(Intent(this, Profile::class.java))
             finish()
         }
+
+        var userData = getUserData()
+        binding.etNama.setText(userData["nama"])
+        binding.etEmail.setText(userData["email"])
+        binding.etTglLahir.setText(userData["tgl_lahir"])
+        binding.etBeratBadan.setText(userData["berat_badan"])
+        binding.etTinggiBadan.setText(userData["tinggi_badan"])
+
     }
+
+    private fun updateProfil() {
+        TODO("BIKIN FUNCTION BUAT UPDATE PROFILE")
+
+
+        startActivity(Intent(this, Profile::class.java))
+        finish()
+    }
+
+    private fun getUserData(): Map<String, String> {
+        val sharedPreferences: SharedPreferences =
+            getSharedPreferences("localData", Context.MODE_PRIVATE)
+
+        val gson = Gson()
+        val json = sharedPreferences.getString("userData", "")
+
+        val type = object : TypeToken<Map<String, String>>() {}.type
+        return gson.fromJson(json, type) ?: emptyMap()
+    }
+
+
 }
