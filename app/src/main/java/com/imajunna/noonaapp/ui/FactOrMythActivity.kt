b505ab380060889dd3cbf456d80c47a15b571a1a
junnatun <junsa02@gmail.com>
2023-12-12 14:53:19 +0700

    fix error

--- app/src/main/java/com/imajunna/noonaapp/ui/FactOrMythActivity.kt
+++ app/src/main/java/com/imajunna/noonaapp/ui/FactOrMythActivity.kt
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.imajunna.noonaapp.R
 import com.imajunna.noonaapp.databinding.ActivityDashboardBinding
+import com.imajunna.noonaapp.databinding.ActivityFactOrMythBinding
 
 class FactOrMythActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivityFactOrMythBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_fact_or_myth)
 
-        binding = FactOrMythActivityBinding.inflate(layoutInflater)
+        binding = ActivityFactOrMythBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 //        binding.btnLessons.setOnClickListener {
