6dcc420ab08aaf8b288a8da6763240b62641e03d
junnatun <junsa02@gmail.com>
2023-12-12 14:38:25 +0700

    create fun send journal

--- app/src/main/java/com/imajunna/noonaapp/ui/AddJournal.kt
+++ app/src/main/java/com/imajunna/noonaapp/ui/AddJournal.kt
@@ -6,69 +6,123 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.RadioButton
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import com.imajunna.noonaapp.R
 import com.imajunna.noonaapp.databinding.ActivityAddJournalBinding
 import com.imajunna.noonaapp.databinding.ActivityLoginPageBinding
 import com.imajunna.noonaapp.databinding.ActivityPeriodJournalBinding
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class AddJournal : AppCompatActivity() {
     private lateinit var binding: ActivityAddJournalBinding
+    private lateinit var db:FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddJournalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db=Firebase.firestore
+
         binding.btnBack.setOnClickListener{
             startActivity(Intent(this,PeriodJournal::class.java))
             finish()
         }
 
-        binding.rgMood.setOnCheckedChangeListener { radioGroup, checkedId ->
-            val selectedMood: RadioButton = findViewById(checkedId)
-            val rbMood0: RadioButton = findViewById(R.id.rb_mood_0)
-            val rbMood1: RadioButton = findViewById(R.id.rb_mood_1)
-            val rbMood2: RadioButton = findViewById(R.id.rb_mood_2)
-            val rbMood3: RadioButton = findViewById(R.id.rb_mood_3)
-            val rbMood4: RadioButton = findViewById(R.id.rb_mood_4)
 
-//            Log.d("RB MOOD", checkedId.toString())
 
-            selectedMood.backgroundTintMode = null
+        binding.btnSaveJournal.setOnClickListener {
+            //HARI
+            var hari = binding.etDate.text.toString()
 
-            if (checkedId != 2131362398) {
-                rbMood0.backgroundTintMode = PorterDuff.Mode.MULTIPLY
-            }
-            if (checkedId != 2131362399) {
-                rbMood1.backgroundTintMode = PorterDuff.Mode.MULTIPLY
-            }
-            if (checkedId != 2131362400) {
-                rbMood2.backgroundTintMode = PorterDuff.Mode.MULTIPLY
-            }
-            if (checkedId != 2131362401) {
-                rbMood3.backgroundTintMode = PorterDuff.Mode.MULTIPLY
-            }
-            if (checkedId != 2131362402) {
-                rbMood4.backgroundTintMode = PorterDuff.Mode.MULTIPLY
-            }
+            //MENS
+            var mens = binding.swMens.isChecked.toString()
 
-            binding.btnSaveJournal.setOnClickListener {
-                var hari = binding.etDate.text.toString()
-                var mens = binding.swMens.isChecked.toString()
-                var mood = binding.rgMood.checkedRadioButtonId.toString()
-                var cairan = binding.rgCairan.checkedRadioButtonId.toString()
-                var aliran = binding.rgAliran.checkedRadioButtonId.toString()
-                var gejala = binding.rgGejala.checkedRadioButtonId.toString()
-                var suhu = binding.etSuhu.text.toString()
-                var minum = binding.etMinum.text.toString()
-                var olahraga = binding.rgOlahraga.checkedRadioButtonId.toString()
+            //MOOD
+            var moodRadioID = binding.rgMood.checkedRadioButtonId
+            var moodRadio:RadioButton=findViewById(moodRadioID)
+            var moodValue = moodRadio.tag.toString()
 
-                Log.d("JOURNAL VALUE", "${hari} | ${mens} | ${mood} | ${cairan} | ${aliran} | ${gejala} | ${suhu} | ${minum} | ${olahraga}")
-            }
+            //CAIRAN
+            var cairanRadioID = binding.rgCairan.checkedRadioButtonId
+            var cairanRadio: RadioButton = findViewById(cairanRadioID)
+            var cairanValue = cairanRadio.text.toString()
+
+            //ALIRAN
+            var aliranRadioID = binding.rgAliran.checkedRadioButtonId
+            var aliranRadio:RadioButton = findViewById(aliranRadioID)
+            var aliranValue = aliranRadio.text.toString()
+
+            //GEJALA
+            var gejalaRadioID = binding.rgGejala.checkedRadioButtonId
+            var gejalaRadio: RadioButton = findViewById(gejalaRadioID)
+            var gejalaValue = gejalaRadio.text.toString()
+
+            //SUHU
+            var suhu = binding.etSuhu.text.toString()
+
+            //MINUM
+            var minum = binding.etMinum.text.toString()
+
+            //OLAHRAGA
+            var olahragaRadioID = binding.rgOlahraga.checkedRadioButtonId
+            var olahragaRadio: RadioButton = findViewById(olahragaRadioID)
+            var olahragaValue = olahragaRadio.text.toString()
+
+            Log.d("JOURNAL VALUE", "${hari} | ${mens} | ${moodValue} | ${cairanValue} | ${aliranValue} | ${gejalaValue} | ${suhu} | ${minum} | ${olahragaValue}")
+            sendJournal(hari,mens,moodValue,cairanValue,aliranValue,gejalaValue,suhu,minum,olahragaValue)
         }
 
-
-
-
     }
+    fun sendJournal(hari:String, mens:String, mood:String, cairan:String, aliran:String, gejala:String, suhu:String, minum:String, olahraga:String){
+        //GET USER EMAIL FROM AUTH
+        val userEmail = FirebaseAuth.getInstance().currentUser?.email
+
+        var journalData = hashMapOf(
+            "email" to userEmail,
+            "hari" to hari,
+            "mens" to mens,
+            "mood" to mood,
+            "cairan" to cairan,
+            "aliran" to aliran,
+            "gejala" to gejala,
+            "suhu" to suhu,
+            "minum" to minum,
+            "olahraga" to olahraga
+        )
+
+        //DATE TIME
+        var currDate = Date()
+        var dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
+        var formattedDate = dateFormat.format(currDate)
+
+        //CREATE ID JOURNAL DOC
+        var docID = userEmail+formattedDate
+
+        db.collection("journals").document(docID)
+            .set(journalData)
+            .addOnSuccessListener {
+                Toast.makeText(
+                    baseContext,
+                    "Add Period Journal Success!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                startActivity(Intent(this,PeriodJournal::class.java))
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    baseContext,
+                    "Add Period Journal Failed!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+    }
+
 }
